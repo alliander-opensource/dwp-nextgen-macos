321d1d5f581e86194d70cb91c4aa4deda4cc295e
John de Vries <john.de.vries1@alliander.com>
2024-09-11 19:37:08 +0200

    Update dockutil_settings.sh

--- Baseline/scripts/dockutil_settings.sh
+++ Baseline/scripts/dockutil_settings.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-                $DOCKUTIL --add '/Applications/Microsoft Outlook.app' --no-restart $UserPlist
-                $DOCKUTIL --add '/Applications/Microsoft Word.app' --no-restart $UserPlist
-                $DOCKUTIL --add '/Applications/Microsoft Excel.app' --no-restart $UserPlist
-                $DOCKUTIL --add '/Applications/Microsoft Powerpoint.app' --no-restart $UserPlist
-                $DOCKUTIL --add '/Applications/Microsoft Teams.app' --no-restart $UserPlist
-            	$DOCKUTIL --add '/Applications/Microsoft Edge.app' --no-restart $UserPlist
-                $DOCKUTIL --add '/Applications/Company Portal.app' --no-restart $UserPlist
+DOCKUTIL=/usr/local/bin/dockutil
 
-            ConfigureDefaultDock
-            Killall Dock
+# add items to dock
+$DOCKUTIL --add '/Applications/Microsoft Outlook.app' --no-restart $UserPlist
+$DOCKUTIL --add '/Applications/Microsoft Word.app' --no-restart $UserPlist
+$DOCKUTIL --add '/Applications/Microsoft Excel.app' --no-restart $UserPlist
+$DOCKUTIL --add '/Applications/Microsoft Powerpoint.app' --no-restart $UserPlist
+$DOCKUTIL --add '/Applications/Microsoft Teams.app' --no-restart $UserPlist
+$DOCKUTIL --add '/Applications/Microsoft Edge.app' --no-restart $UserPlist
+$DOCKUTIL --add '/Applications/Company Portal.app' --no-restart $UserPlist
