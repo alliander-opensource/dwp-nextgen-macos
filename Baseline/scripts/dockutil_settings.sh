2de2f3628929ef67a7ec49aa69ecc5b8202b8b09
John de Vries <john.de.vries1@alliander.com>
2024-09-11 19:35:39 +0200

    Upload

--- Baseline/scripts/dockutil_settings.sh
+++ Baseline/scripts/dockutil_settings.sh
@@ -1,9 +1,5 @@
-            UserPlist=/Users/$currentUser/Library/Preferences/com.apple.dock.plist
-            
-            ConfigureDefaultDock() {
-                
-                DOCKUTIL=/usr/local/bin/dockutil
-                
+#!/bin/bash
+
                 $DOCKUTIL --add '/Applications/Microsoft Outlook.app' --no-restart $UserPlist
                 $DOCKUTIL --add '/Applications/Microsoft Word.app' --no-restart $UserPlist
                 $DOCKUTIL --add '/Applications/Microsoft Excel.app' --no-restart $UserPlist
@@ -11,7 +7,6 @@
                 $DOCKUTIL --add '/Applications/Microsoft Teams.app' --no-restart $UserPlist
             	$DOCKUTIL --add '/Applications/Microsoft Edge.app' --no-restart $UserPlist
                 $DOCKUTIL --add '/Applications/Company Portal.app' --no-restart $UserPlist
-            }
-            
+
             ConfigureDefaultDock
             Killall Dock
