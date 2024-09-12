a96ae086590e03324cc75d86d9668e0cb9a0f4da
John de Vries <john.de.vries1@alliander.com>
2024-09-12 17:10:18 +0200

    Update dockutil_settings.sh

--- Baseline/scripts/dockutil_settings.sh
+++ Baseline/scripts/dockutil_settings.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+currentUser=$( echo "show State:/Users/ConsoleUser" | scutil | awk '/Name :/ { print $3 }' )
+dockPrefs="/Users/$currentUser/Library/Preferences/com.apple.dock.plist"
+uid=$(id -u "$currentUser")
+
+runAsUser() {  
+	if [ "$currentUser" != "loginwindow" ]; then
+		launchctl asuser "$uid" sudo -u "$currentUser" "$@"
+	else
+		echo "no user logged in"
+	fi
+}
+
 # add items to dock
 /usr/local/bin/dockutil --add  /Applications/Microsoft Outlook.app
 /usr/local/bin/dockutil --add  /Applications/Microsoft Word.app
@@ -8,5 +20,7 @@
 /usr/local/bin/dockutil --add  /Applications/Microsoft Teams.app
 /usr/local/bin/dockutil --add  /Applications/Microsoft Edge.app
 /usr/local/bin/dockutil --add  /Applications/Company Portal.app
+
+killall Dock
 
 exit 0 
