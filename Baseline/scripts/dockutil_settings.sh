32ecabf3661a3177705adbec3b28afd636d469c9
John de Vries <john.de.vries1@alliander.com>
2024-09-12 16:57:43 +0200

    Update dockutil_settings.sh

--- Baseline/scripts/dockutil_settings.sh
+++ Baseline/scripts/dockutil_settings.sh
@@ -1,39 +1,12 @@
 #!/bin/bash
 
-currentUser=$( echo "show State:/Users/ConsoleUser" | scutil | awk '/Name :/ { print $3 }' )
-dockPrefs="/Users/$currentUser/Library/Preferences/com.apple.dock.plist"
-uid=$(id -u "$currentUser")
-
-runAsUser() {  
-	if [ "$currentUser" != "loginwindow" ]; then
-		launchctl asuser "$uid" sudo -u "$currentUser" "$@"
-	else
-		echo "no user logged in"
-	fi
-}
-
-addToDock() {  
-	if [ -d "$@" ]; then
-		inDock=0
-        counter=0
-		while [ "$inDock" == "0" ]; do 
-			runAsUser /usr/local/bin/dockutil --add "$@" /Users/$currentUser > /dev/null 2>&1 || true
-			grepterm=$(echo "$@" | sed 's/ /%20/g')
-			inDock=$(defaults read /Users/$currentUser/Library/Preferences/com.apple.dock.plist persistent-apps | grep -c "$grepterm")
-			counter=$[$counter+1]
-		done
-	else
-		echo "$@ is not installed, so will not be added to the dock"
-	fi
-}
-
 # add items to dock
-addToDock '/Applications/Microsoft Outlook.app'
-addToDock '/Applications/Microsoft Word.app'
-addToDock '/Applications/Microsoft Excel.app'
-addToDock '/Applications/Microsoft Powerpoint.app'
-addToDock '/Applications/Microsoft Teams.app'
-addToDock '/Applications/Microsoft Edge.app'
-addToDock '/Applications/Company Portal.app'
+/usr/local/bin/dockutil --add  /Applications/Microsoft Outlook.app
+/usr/local/bin/dockutil --add  /Applications/Microsoft Word.app
+/usr/local/bin/dockutil --add  /Applications/Microsoft Excel.app
+/usr/local/bin/dockutil --add  /Applications/Microsoft Powerpoint.app
+/usr/local/bin/dockutil --add  /Applications/Microsoft Teams.app
+/usr/local/bin/dockutil --add  /Applications/Microsoft Edge.app
+/usr/local/bin/dockutil --add  /Applications/Company Portal.app
 
 exit 0 
