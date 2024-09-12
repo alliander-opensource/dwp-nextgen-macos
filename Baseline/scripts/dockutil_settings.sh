a551a48de60bd67870c60e0580903624aaa0f993
John de Vries <john.de.vries1@alliander.com>
2024-09-12 10:38:29 +0200

    Update dockutil_settings.sh

--- Baseline/scripts/dockutil_settings.sh
+++ Baseline/scripts/dockutil_settings.sh
@@ -1,12 +1,42 @@
 #!/bin/bash
 
-DOCKUTIL=/usr/local/bin/dockutil
+currentUser=$( echo "show State:/Users/ConsoleUser" | scutil | awk '/Name :/ { print $3 }' )
+dockPrefs="/Users/$currentUser/Library/Preferences/com.apple.dock.plist"
+uid=$(id -u "$currentUser")
+
+runAsUser() {  
+	if [ "$currentUser" != "loginwindow" ]; then
+		launchctl asuser "$uid" sudo -u "$currentUser" "$@"
+	else
+		echo "no user logged in"
+	fi
+}
+
+addToDock() {  
+	if [ -d "$@" ]; then
+		inDock=0
+        counter=0
+		while [ "$inDock" == "0" ]; do 
+			runAsUser /usr/local/bin/dockutil --add "$@" /Users/$currentUser > /dev/null 2>&1 || true
+			grepterm=$(echo "$@" | sed 's/ /%20/g')
+			inDock=$(defaults read /Users/$currentUser/Library/Preferences/com.apple.dock.plist persistent-apps | grep -c "$grepterm")
+			counter=$[$counter+1]
+            echo "$@: Status:$inDock Counter $counter" >> /private/var/log/Dockutil.log
+            if [ "$counter" -gt "999" ]; then
+            	 echo "$@: Failed" >> /private/var/log/Dockutil.log
+                 inDock="0"
+            fi
+		done
+	else
+		echo "$@ is not installed, so will not be added to the dock"
+	fi
+}
 
 # add items to dock
-$DOCKUTIL --add '/Applications/Microsoft Outlook.app' --no-restart
-$DOCKUTIL --add '/Applications/Microsoft Word.app' --no-restart
-$DOCKUTIL --add '/Applications/Microsoft Excel.app' --no-restart
-$DOCKUTIL --add '/Applications/Microsoft Powerpoint.app' --no-restart
-$DOCKUTIL --add '/Applications/Microsoft Teams.app' --no-restart
-$DOCKUTIL --add '/Applications/Microsoft Edge.app' --no-restart
-$DOCKUTIL --add '/Applications/Company Portal.app' --no-restart
+addToDock '/Applications/Microsoft Outlook.app' --no-restart
+addToDock '/Applications/Microsoft Word.app' --no-restart
+addToDock '/Applications/Microsoft Excel.app' --no-restart
+addToDock '/Applications/Microsoft Powerpoint.app' --no-restart
+addToDock '/Applications/Microsoft Teams.app' --no-restart
+addToDock '/Applications/Microsoft Edge.app' --no-restart
+addToDock '/Applications/Company Portal.app' --no-restart
