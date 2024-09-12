bcd0c529c744a85b3455be0da4f9c40d91685a14
John de Vries <john.de.vries1@alliander.com>
2024-09-12 21:37:35 +0200

    v1 upload

--- Baseline/scripts/dockutil_settings.sh
+++ Baseline/scripts/dockutil_settings.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-currentUser=$( echo "show State:/Users/ConsoleUser" | scutil | awk '/Name :/ { print $3 }' )
-dockPrefs="/Users/$currentUser/Library/Preferences/com.apple.dock.plist"
-uid=$(id -u "$currentUser")
-
-runAsUser() {  
-	if [ "$currentUser" != "loginwindow" ]; then
-		launchctl asuser "$uid" sudo -u "$currentUser" "$@"
-	else
-		echo "no user logged in"
-	fi
-}
-
 # add items to dock
 /usr/local/bin/dockutil --add  /Applications/Microsoft Outlook.app
 /usr/local/bin/dockutil --add  /Applications/Microsoft Word.app
