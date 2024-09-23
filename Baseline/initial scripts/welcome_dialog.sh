8979e4b67f1e73c29722e84ac87f4bbdff8dc0e9
John de Vries <john.de.vries1@alliander.com>
2024-09-23 12:53:41 +0200

    Update welcome_dialog.sh

--- Baseline/initial scripts/welcome_dialog.sh
+++ Baseline/initial scripts/welcome_dialog.sh
@@ -6,7 +6,7 @@ dialog='/usr/local/bin/dialog'
 # get loggedInUser user
 loggedInUser=$( /usr/sbin/scutil <<< "show State:/Users/ConsoleUser" | /usr/bin/awk '/Name :/ { print $3 }' )
 loggedInUserID=$( /usr/bin/id -u "$loggedInUser" )
-realName=$(dscl . -read /Users/$loggedInUser RealName | awk -F": " '{print $2}')
+realName=$(/usr/libexec/PlistBuddy -c "print :dsAttrTypeStandard\:RealName:0" /dev/stdin <<< $(dscl -plist . read /Users/$loggedInUser RealName))
 
 runAsUser() {
   if [ "$loggedInUser" != "loginwindow" ]; then
@@ -24,7 +24,7 @@ runAsUser $dialog \
 --ontop \
 --title "Gefeliciteerd met je nieuwe MacBook!" \
 --icon "https://github.com/alliander-opensource/dwp-nextgen-macos/blob/main/Baseline/pictures/logos/logo.png?raw=true" \
---message "Hoi$realName,\n\nWe gaan je helpen om alles in te stellen en de juiste programma's te installeren.\n\nAls de installatie klaar is, zie je een knop met **Restart now**. Klik daarop om je MacBook opnieuw op te starten.\n\n Dit kan even duren, dus heb geduld...." \
+--message "Hoi $realName,\n\nWe gaan je helpen om alles in te stellen en de juiste programma's te installeren.\n\nAls de installatie klaar is, zie je een knop met **Herstart**. Klik daarop om je MacBook opnieuw op te starten en de installatie af te ronden.\n\n Dit kan even duren, dus heb geduld...." \
 --messagefont "size=14" \
 --bannerimage "https://github.com/alliander-opensource/dwp-nextgen-macos/blob/main/Baseline/pictures/logos/banner.png?raw=true" \
 --button1text "Aan de slag" \
