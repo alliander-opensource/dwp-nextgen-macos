eeb1c802ec05f96791cc853e81bede45d68f11b3
John de Vries <john.de.vries1@alliander.com>
2024-10-09 12:28:26 +0200

    demote admin deleted

--- Baseline/scripts/touch_conditionfile.sh
+++ Baseline/scripts/touch_conditionfile.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
 
-########## Demote admin user ##########
-
-# Get a list of local accounts (UID > 500) excluding specific ones
-localAccts=$(dscl . list /Users UniqueID | awk '$2 > 500 {print $1}' | grep -vE 'mgtcltadmin|_mbsetupuser')
-
-# Loop through the list of accounts and remove them from the admin group
-while IFS= read -r account; do
-    echo "Removing $account from the local admin group (if present)..."
-    dseditgroup -o edit -d "$account" admin
-    if [ $? -eq 0 ]; then
-        echo "Successfully removed $account from the admin group."
-    else
-        echo "Failed to remove $account from the admin group, or account is not in the group."
-        exit 1
-    fi
-done <<< "$localAccts"
-
 ########## Touch condition file ##########
 
 if [[ ! -f "/var/log/org.alliander.log" ]]; then
