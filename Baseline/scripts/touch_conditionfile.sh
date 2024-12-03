de059cfccf2101641df0303952a0f31649302dd4
John de Vries <john.de.vries1@alliander.com>
2024-12-03 10:57:58 +0100

    Update touch_conditionfile.sh

--- Baseline/scripts/touch_conditionfile.sh
+++ Baseline/scripts/touch_conditionfile.sh
@@ -2,11 +2,6 @@
 
 ########## Touch condition file ##########
 
-# Check if /var/log/org.alliander.log exists, if not, create it
-if [[ ! -f "/var/log/org.alliander.log" ]]; then
-    touch "/var/log/org.alliander.log"
-fi
-
 # Check if /Library/Application Support/Alliander/Logs/Baseline_complete.log exists, if not, create it
 if [[ ! -f "/Library/Application Support/Alliander/Logs/Baseline_complete.log" ]]; then
     touch "/Library/Application Support/Alliander/Logs/Baseline_complete.log"
